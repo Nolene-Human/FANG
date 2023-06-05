64a67f51ebfd470b70e5130a18cb9a689ed916f5
Nolene Human <108449986+Nolene-Human@users.noreply.github.com>
2023-06-05 19:24:35 +1200

    Delete xxx_Delete directory

--- xxx_Delete/based_on_authentication/Adminpage.py
+++
@@ -1,8 +0,0 @@
-import streamlit as lit
-
-def admin():
-
-    lit.title("with Great Power comes Great Responsibily")
-    lit.sidebar.success("Admin")
-
-    lit.write("Welcome Admin to your new page")
